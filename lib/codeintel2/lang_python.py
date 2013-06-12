5d25b1e363b0e1433e539314c1d57e8bc766a462
German M. Bravo <german.mb@deipi.com>
2013-06-12 18:33:30 -0500

    CodeIntel2: Calltips for function parameters when hitting comma

--- lib/codeintel2/lang_python.py
+++ lib/codeintel2/lang_python.py
@@ -728,7 +728,7 @@ class PythonBuffer(CitadelBuffer):
 
         # Quick out if the preceding char isn't a trigger char.
         # Note: Cannot use this now that we have a 2-char locals trigger.
-        # if last_char not in " .(@_":
+        # if last_char not in " .(@_,":
         #    if DEBUG:
         #        print "trg_from_pos: no: %r is not in ' .(@'_" % last_char
         #    return None
@@ -1015,6 +1015,18 @@ class PythonBuffer(CitadelBuffer):
                 if DEBUG:
                     print "trg_from_pos: no: no chars preceding '('"
             return None
+        elif last_char == ',':
+            working_text = accessor.text_range(max(0, last_pos - 200), last_pos)
+            line = self._last_logical_line(working_text)
+            if line:
+                last_bracket = line.rfind("(")
+                if last_bracket >= 0:
+                    pos = (pos - (len(line) - last_bracket))
+                    return Trigger(self.lang, TRG_FORM_CALLTIP,
+                                   "call-signature", pos, implicit)
+                return None
+            else:
+                return None
 
         elif pos >= 2 and style in (self.identifier_style, self.keyword_style):
             # 2 character trigger for local symbols
